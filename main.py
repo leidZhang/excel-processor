ef447bcb67a4003ef829956fd699d3b45c99a1db
leidZhang <113314347+leidZhang@users.noreply.github.com>
2023-05-24 18:19:46 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class ExcelProcessor:
     def __init__(self):
         self.df = None
 
-    def openFile(self): 
+    def openFile(self, text): 
         filePath = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
 
         if filePath: 
@@ -15,9 +15,150 @@ class ExcelProcessor:
                 print("Openning file...")
                 self.df = pd.read_excel(filePath)
                 self.filePath = filePath
-                print(self.df)
+                dfStr = self.df.to_string(index=False)
+                text.insert(tk.END, dfStr)
             except pd.errors.ParserError: 
                 messagebox.showerror("Error", "Please open a valid Excel File")
+
+    def initHomeMenu(self): 
+        print("Main menu")
+        # clear GUI
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        # generate buttons
+        scrollbar = tk.Scrollbar(root)
+        text = tk.Text(root, yscrollcommand=scrollbar.set)
+        openButton = tk.Button(root, text="Open Excel file", command=lambda: self.openFile(text))
+        insertButton = tk.Button(root, text="Insert Data", command=self.insertData)
+        searchButton = tk.Button(root, text="Search Data", command=self.initSearch)
+
+        openButton.place(x=10, y=10) 
+        insertButton.place(x=120, y=10) 
+        searchButton.place(x=210, y=10) 
+        scrollbar.place(x=380, y=50, height=230) 
+        text.place(x=10, y=50, width=450, height=230) 
+        scrollbar.config(command=text.yview)
+
+        if self.df is not None:
+            dfStr = self.df.to_string(index=False)
+            text.insert(tk.END, dfStr)
+
+    def handleInsertData(self, colNames, inputEntries, resLabel): 
+        data = {}
+        for colName in colNames:
+            entryValue = inputEntries[colName].get()
+            data[colName] = entryValue
+
+        # insert data to the DataFrame
+        newDf = pd.DataFrame (data, index=[0]) 
+        self.df = pd.concat([self.df, newDf], ignore_index=True) 
+        print("Data inserted successfully!")
+        print(self.df)
+
+        # save updated DataFrame to Excel File
+        self.df.to_excel(self.filePath, index=False)
+        resLabel.configure(text="Excel file updated!")
+        
+        # clear form
+        for entry in inputEntries.values():
+            entry.delete(0, tk.END)
+
+    def insertData(self): 
+        print("Insert data")
+        if self.df is None: 
+            print("Please open an excel file")
+            return 
+        
+        # clear layout
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        # generate form
+        initX = 10
+        initY = 10
+        cnt = 0
+        colNames = self.df.columns.tolist()
+        inputEntries = {}
+        for colName in colNames:
+            label = tk.Label(self.root, text=colName)
+            entry = tk.Entry(self.root)
+            label.place(x=initX, y=initY + cnt * 30)
+            entry.place(x=initX + 100, y=initY + cnt * 30)   
+            cnt += 1 
+            
+            inputEntries[colName] = entry
+
+        # generate other components
+        resLablel = tk.Label(self.root, text="")        
+        submitButton = tk.Button(self.root, text="Submit", command=lambda: self.handleInsertData(colNames, inputEntries, resLablel))
+        cancelButton = tk.Button(self.root, text="Cancel", command=self.initHomeMenu)
+        submitButton.place(x=initX + 100, y=initY + cnt * 30)
+        cancelButton.place(x=initX + 160, y=initY + cnt * 30)
+        resLablel.place(x=initX, y=initY + (cnt + 1) * 30)
+
+    def handleAccurateSearch(self, colNames, inputEntries, screen): 
+        screen.delete('1.0', tk.END)
+        print("Searching...")
+        # get user input 
+        inputData = {}
+        for colName in colNames:
+            entryValue = inputEntries[colName].get()
+            inputData[colName] = entryValue
+
+        # accurate search 
+        matchingRows = []
+        for i, row in self.df.iterrows():
+            match = True
+            for colName, value in inputData.items(): 
+                print("Searching: " + colName + ", " + value)
+                print("Getting: " + colName + ", " + str(row[colName]))
+                if str(inputData[colName]) == "":
+                    print("The column " + colName + "is empty") 
+                    continue
+                if str(row[colName]) != value: 
+                    match = False
+                    break
+            if match: 
+                matchingRows.append(row)
+
+        print("Search result: ")
+        matchingDf = pd.DataFrame(matchingRows)
+        matchingStr = matchingDf.to_string(index=False)
+        screen.insert(tk.END, matchingStr)
+
+    def accurateSearch(self): 
+        print("Switch to accurate search")
+        # clear GUI
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        # generate form
+        initX = 10
+        initY = 10
+        cnt = 0
+        colNames = self.df.columns.tolist()
+        inputEntries = {}
+        for colName in colNames:
+            label = tk.Label(self.root, text=colName)
+            entry = tk.Entry(self.root)
+            label.place(x=initX, y=initY + cnt * 30)
+            entry.place(x=initX + 100, y=initY + cnt * 30)     
+            cnt += 1
+            
+            inputEntries[colName] = entry
+
+        # generate other components
+        scrollbar = tk.Scrollbar(root)
+        text = tk.Text(root, yscrollcommand=scrollbar.set)
+        searchButton = tk.Button(self.root, text="Search", command=lambda: self.handleAccurateSearch(colNames, inputEntries, text))
+        cancelButton = tk.Button(self.root, text="Cancel", command=self.initSearch)
+
+        searchButton.place(x=initX + 100, y=initY + cnt * 30)
+        cancelButton.place(x=initX + 160, y=initY + cnt * 30)
+        scrollbar.place(x=initX + 370, y=initY + cnt * 30 + 40, height=230)
+        text.place(x=initX, y=initY + cnt * 30 + 40, width=450, height=230)
+        scrollbar.config(command=text.yview)
 
     def handleFuzzySearch(self, entry, screen):
         screen.delete('1.0', tk.END)
@@ -41,16 +182,12 @@ class ExcelProcessor:
 
         dfStr = res.to_string(index=False)
         screen.insert(tk.END, dfStr)
-    
-    def accurateSearch(self): 
-        # impl accurate search
-        return None
 
-    def search(self):
+    def initSearch(self):
+        print("Search data")
         if self.df is None: 
             print("Please open an excel file")
             return 
-        print("Search data")
         
         # clear GUI
         for widget in self.root.winfo_children():
@@ -59,81 +196,24 @@ class ExcelProcessor:
         # generate layout
         entry = tk.Entry(root)
         scrollbar = tk.Scrollbar(root)
+        label = tk.Label(root, text="Search:")
         text = tk.Text(root, yscrollcommand=scrollbar.set)
-        fuzzySerachButton = tk.Button(root, text="Serach", command=lambda: ep.handleFuzzySearch(entry, text))
-        accurateSearchButton = tk.Button(root, text="Accuarte Serach", command=ep.accurateSearch)
+        fuzzySerachButton = tk.Button(root, text="Serach", command=lambda: self.handleFuzzySearch(entry, text))
+        accurateSearchButton = tk.Button(root, text="Accuarte Serach", command=self.accurateSearch)
         cancelButton = tk.Button(self.root, text="Cancel", command=self.initHomeMenu)
 
-        entry.pack()
-        fuzzySerachButton.pack()
-        accurateSearchButton.pack()
-        cancelButton.pack()
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        text.pack(side=tk.LEFT, fill=tk.BOTH)
+        label.place(x=10, y=15)
+        entry.place(x=80, y=15)
+        fuzzySerachButton.place(x=240, y=10)
+        accurateSearchButton.place(x=300, y=10)
+        cancelButton.place(x=415, y=10)
+        scrollbar.place(x=380, y=50, height=230) 
+        text.place(x=10, y=50, width=450, height=230) 
         scrollbar.config(command=text.yview)
-    
-    def initHomeMenu(self): 
-        # clear GUI
-        for widget in self.root.winfo_children():
-            widget.destroy()
-
-        # generate buttons
-        openButton = tk.Button(root, text="Open Excel file", command=ep.openFile)
-        openButton.pack()
-        insertButton = tk.Button(root, text="Insert Data", command=ep.insertData)
-        insertButton.pack()
-        searchButton = tk.Button(root, text="Search Data", command=ep.search)
-        searchButton.pack()
-
-    def handleInsertData(self, colNames, inputEntries): 
-        data = {}
-        for colName in colNames:
-            entryValue = inputEntries[colName].get()
-            data[colName] = entryValue
-
-        # insert data to the DataFrame
-        newDf = pd.DataFrame (data, index=[0]) 
-        self.df = pd.concat([self.df, newDf], ignore_index=True) 
-        print("Data inserted successfully!")
-        print(self.df)
-
-        # save updated DataFrame to Excel File
-        self.df.to_excel(self.filePath, index=False)
-        print("Excel file updated!")
-        
-        # clear form
-        for entry in inputEntries.values():
-            entry.delete(0, tk.END)
-
-    def insertData(self): 
-        if self.df is None: 
-            print("Please open an excel file")
-            return 
-        print("Insert data")
-        
-        # clear layout
-        for widget in self.root.winfo_children():
-            widget.destroy()
-
-        # generate form
-        colNames = self.df.columns.tolist()
-        inputEntries = {}
-        for colName in colNames:
-            label = tk.Label(self.root, text=colName)
-            entry = tk.Entry(self.root)
-            label.pack()
-            entry.pack()    
-            
-            inputEntries[colName] = entry
-
-        # generate buttons
-        submitBtn = tk.Button(self.root, text="Submit", command=lambda: self.handleInsertData(colNames, inputEntries))
-        cancelBtn = tk.Button(self.root, text="Cancel", command=self.initHomeMenu)
-        submitBtn.pack()
-        cancelBtn.pack()
 
 root = tk.Tk()
-root.minsize(width=400, height=300)
+root.title("Simple Excel Processor")
+root.minsize(width=470, height=300)
 
 ep = ExcelProcessor()
 ep.root = root
